185992d865b27bd65440fcb649ea4335dc975f06
Ronald Roberts <djamadeus1@gmail.com>
2024-09-27 22:48:17 -0400

    Yeah Okaaaay

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St",
+    city: "Los Angeles"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,        
+        [key]: value       
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;       
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: _, ...newEmployee } = employee; 
+    return newEmployee; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee; 
+}
+
+
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, "city", "New York");
+console.log(updatedEmployee);  
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "city", "Los Angeles");
+console.log(employee);  
+
+let employeeWithoutCity = deleteFromEmployeeByKey(employee, "city");
+console.log(employeeWithoutCity); 
+
+destructivelyDeleteFromEmployeeByKey(employee, "city");
+console.log(employee); 
